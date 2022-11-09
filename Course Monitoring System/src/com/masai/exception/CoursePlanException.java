075ca5276984b9cd19682801b62d2d6ed96c9d74
Abhijeet Ojha <avie.just@gmail.com>
2022-11-09 13:00:24 +0530

    pusing day-2 code

---
+++ Course Monitoring System/src/com/masai/exception/CoursePlanException.java
@@ -0,0 +1,13 @@
+package com.masai.exception;
+
+public class CoursePlanException extends Exception {
+
+	public CoursePlanException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CoursePlanException(String message) {
+		super(message);
+	}
+
+}
---
+++ Course Monitoring System/src/com/masai/exception/FacultyException.java
@@ -0,0 +1,13 @@
+package com.masai.exception;
+
+public class FacultyException extends Exception{
+	
+	public FacultyException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public FacultyException(String message) {
+		super(message);
+	}
+
+}
